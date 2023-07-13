4e9ac2758744d9dc3d98c6ba6d3ebf219a6d4daa
sakshi verma <sakshiverma7879@gmail.com>
2023-07-13 20:34:05 +0530

    done3

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -36,8 +36,8 @@ something close to the meaning of life.”</p>
                         <div className="col-md-6 col-lg-4 col-12 ft-3">
                             <h5>Quick Links</h5>
                             <p><i class="fa-solid fa-phone-volume"></i> +92 3121324083</p>
-                            <p><i class="fa-solid fa-envelope"></i> waleedishfaq1515@gmail.com</p>
-                            <p><i class="fa-solid fa-paper-plane"></i> Abbottabad, Pakistan.</p>
+                            <p><i class="fa-solid fa-envelope"></i> sakshiverma8085@gmail.com</p>
+                            <p><i class="fa-solid fa-paper-plane"></i> Delhi , India</p>
                         </div>
                     </div>
                 </div>
